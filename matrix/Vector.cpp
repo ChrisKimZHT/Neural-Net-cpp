3d666e1aaf8e0d321f568bb5523d33d0e3a44dc2
ChrisKim <1515748548@qq.com>
2023-06-06 18:19:44 +0800

    [feat]: 向量类

---
+++ matrix/Vector.cpp
@@ -0,0 +1,8 @@
+//
+// Created by ChrisKim on 2023/6/6.
+//
+
+#include "Vector.h"
+
+Vector::Vector(int dimension, double val)
+        : Matrix(dimension, 1, val) {}
---
+++ matrix/Vector.h
@@ -0,0 +1,18 @@
+//
+// Created by ChrisKim on 2023/6/6.
+//
+
+#ifndef ANN_VECTOR_H
+#define ANN_VECTOR_H
+
+#include "Matrix.h"
+
+class Vector : public Matrix
+{
+public:
+    Vector() = default;
+    explicit Vector(int dimension, double val = 0);
+};
+
+
+#endif //ANN_VECTOR_H
