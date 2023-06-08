8defea93264948423455ebaa331589627791cb06
ChrisKim <1515748548@qq.com>
2023-06-08 13:17:55 +0800

    [feat]: 使用Python可视化数据集

---
+++ test/PlotPlaneData.py
@@ -0,0 +1,25 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+with open("dataset.txt", "r") as f:
+    num_samples = int(f.readline().strip())
+
+    input_shape = tuple(map(int, f.readline().strip().split()))
+    label_shape = tuple(map(int, f.readline().strip().split()))
+
+    x = []
+    y = []
+    for i in range(num_samples):
+        data = list(map(float, f.readline().strip().split()))
+        x_vec = np.array(data[:2])
+        y_vec = np.array(data[2:])
+        x.append(x_vec)
+        y.append(y_vec)
+    x = np.array(x)
+    y = np.array(y)
+
+plt.xlabel("x1")
+plt.ylabel("x2")
+plt.axis("equal")
+plt.scatter(x[:, 0], x[:, 1], c=y)
+plt.show()
