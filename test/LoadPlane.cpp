8b276c956312444b8ef90f48490860262254dc06
ChrisKim <1515748548@qq.com>
2024-04-28 21:48:32 +0800

    [refactor]: 更改生成的数据集格式

--- test/LoadPlane.cpp
+++ test/LoadPlane.cpp
@@ -4,8 +4,7 @@
 
 #include "LoadPlane.h"
 
-std::vector<std::pair<Matrix, Matrix>> circle_type(int size)
-{
+std::pair<std::vector<Matrix>, std::vector<Matrix>> circle_type(int size) {
     // random generator
     auto now = std::chrono::system_clock::now();
     auto now_sec = std::chrono::time_point_cast<std::chrono::seconds>(now);
@@ -13,11 +12,10 @@ std::vector<std::pair<Matrix, Matrix>> circle_type(int size)
     std::default_random_engine generator(timestamp);
     std::normal_distribution<double> distribution(0, 1);
 
-    std::vector<std::pair<Matrix, Matrix>> data;
+    std::vector<Matrix> input, output;
 
     // inner circle
-    for (int i = 0; i < size / 2; i++)
-    {
+    for (int i = 0; i < size / 2; i++) {
         double degree = 2.0 * i / size * 2 * M_PI;
         double x = cos(degree) + 0.5 * distribution(generator);
         double y = sin(degree) + 0.5 * distribution(generator);
@@ -26,12 +24,12 @@ std::vector<std::pair<Matrix, Matrix>> circle_type(int size)
         dot[0][0] = x;
         dot[1][0] = y;
         label[0][0] = 1;
-        data.emplace_back(dot, label);
+        input.emplace_back(dot);
+        output.emplace_back(label);
     }
 
     // outer ring
-    for (int i = 0; i < size / 2; i++)
-    {
+    for (int i = 0; i < size / 2; i++) {
         double degree = 2.0 * i / size * 2 * M_PI;
         double x = 4 * cos(degree) + 0.5 * distribution(generator);
         double y = 4 * sin(degree) + 0.5 * distribution(generator);
@@ -40,13 +38,13 @@ std::vector<std::pair<Matrix, Matrix>> circle_type(int size)
         dot[0][0] = x;
         dot[1][0] = y;
         label[0][0] = -1;
-        data.emplace_back(dot, label);
+        input.emplace_back(dot);
+        output.emplace_back(label);
     }
-    return data;
+    return {input, output};
 }
 
-std::vector<std::pair<Matrix, Matrix>> cluster_type(int size)
-{
+std::pair<std::vector<Matrix>, std::vector<Matrix>> cluster_type(int size) {
     // random generator
     auto now = std::chrono::system_clock::now();
     auto now_sec = std::chrono::time_point_cast<std::chrono::seconds>(now);
@@ -54,11 +52,10 @@ std::vector<std::pair<Matrix, Matrix>> cluster_type(int size)
     std::default_random_engine generator(timestamp);
     std::normal_distribution<double> distribution(0, 1);
 
-    std::vector<std::pair<Matrix, Matrix>> data;
+    std::vector<Matrix> input, output;
 
     // cluster 1
-    for (int i = 0; i < size / 2; i++)
-    {
+    for (int i = 0; i < size / 2; i++) {
         double x = distribution(generator);
         double y = distribution(generator);
         Matrix dot(2, 1);
@@ -66,12 +63,12 @@ std::vector<std::pair<Matrix, Matrix>> cluster_type(int size)
         dot[0][0] = x;
         dot[1][0] = y;
         label[0][0] = 1;
-        data.emplace_back(dot, label);
+        input.emplace_back(dot);
+        output.emplace_back(label);
     }
 
     // cluster 2
-    for (int i = 0; i < size / 2; i++)
-    {
+    for (int i = 0; i < size / 2; i++) {
         double x = 4 + distribution(generator);
         double y = 4 + distribution(generator);
         Matrix dot(2, 1);
@@ -79,14 +76,14 @@ std::vector<std::pair<Matrix, Matrix>> cluster_type(int size)
         dot[0][0] = x;
         dot[1][0] = y;
         label[0][0] = -1;
-        data.emplace_back(dot, label);
+        input.emplace_back(dot);
+        output.emplace_back(label);
     }
 
-    return data;
+    return {input, output};
 }
 
-std::vector<std::pair<Matrix, Matrix>> window_type(int size)
-{
+std::pair<std::vector<Matrix>, std::vector<Matrix>> window_type(int size) {
     // random generator
     auto now = std::chrono::system_clock::now();
     auto now_sec = std::chrono::time_point_cast<std::chrono::seconds>(now);
@@ -94,32 +91,28 @@ std::vector<std::pair<Matrix, Matrix>> window_type(int size)
     std::default_random_engine generator(timestamp);
     std::uniform_real_distribution<double> distribution(-1, 1);
 
-    std::vector<std::pair<Matrix, Matrix>> data;
+    std::vector<Matrix> input, output;
 
-    for (int i = 0; i < size; i++)
-    {
+    for (int i = 0; i < size; i++) {
         double x = distribution(generator);
         double y = distribution(generator);
         Matrix dot(2, 1);
         Matrix label(1, 1);
         dot[0][0] = x;
         dot[1][0] = y;
-        if (x * y >= 0)
-        {
+        if (x * y >= 0) {
             label[0][0] = 1;
-        }
-        else
-        {
+        } else {
             label[0][0] = -1;
         }
-        data.emplace_back(dot, label);
+        input.emplace_back(dot);
+        output.emplace_back(label);
     }
 
-    return data;
+    return {input, output};
 }
 
-std::vector<std::pair<Matrix, Matrix>> whirlpool_type(int size)
-{
+std::pair<std::vector<Matrix>, std::vector<Matrix>> whirlpool_type(int size) {
     // random generator
     auto now = std::chrono::system_clock::now();
     auto now_sec = std::chrono::time_point_cast<std::chrono::seconds>(now);
@@ -127,11 +120,10 @@ std::vector<std::pair<Matrix, Matrix>> whirlpool_type(int size)
     std::default_random_engine generator(timestamp);
     std::normal_distribution<double> distribution(0, 0.4);
 
-    std::vector<std::pair<Matrix, Matrix>> data;
+    std::vector<Matrix> input, output;
 
     // type 1
-    for (int i = 0; i < size / 2; i++)
-    {
+    for (int i = 0; i < size / 2; i++) {
         double degree = 4.0 * i / size * 2 * M_PI;
         double x = degree * cos(degree) + distribution(generator);
         double y = degree * sin(degree) + distribution(generator);
@@ -140,12 +132,12 @@ std::vector<std::pair<Matrix, Matrix>> whirlpool_type(int size)
         dot[0][0] = x;
         dot[1][0] = y;
         label[0][0] = 1;
-        data.emplace_back(dot, label);
+        input.emplace_back(dot);
+        output.emplace_back(label);
     }
 
     // type -1
-    for (int i = 0; i < size / 2; i++)
-    {
+    for (int i = 0; i < size / 2; i++) {
         double degree = 4.0 * i / size * 2 * M_PI;
         double x = degree * cos(degree + 2 * M_PI) + distribution(generator);
         double y = degree * sin(degree + 2 * M_PI) + distribution(generator);
@@ -154,32 +146,23 @@ std::vector<std::pair<Matrix, Matrix>> whirlpool_type(int size)
         dot[0][0] = -x;
         dot[1][0] = -y;
         label[0][0] = -1;
-        data.emplace_back(dot, label);
+        input.emplace_back(dot);
+        output.emplace_back(label);
     }
 
-    return data;
+    return {input, output};
 }
 
-std::vector<std::pair<Matrix, Matrix>> load_plane(int size, const std::string &type)
-{
-    if (type == "circle")
-    {
+std::pair<std::vector<Matrix>, std::vector<Matrix>> load_plane(int size, const std::string &type) {
+    if (type == "circle") {
         return circle_type(size);
-    }
-    else if (type == "cluster")
-    {
+    } else if (type == "cluster") {
         return cluster_type(size);
-    }
-    else if (type == "window")
-    {
+    } else if (type == "window") {
         return window_type(size);
-    }
-    else if (type == "whirlpool")
-    {
+    } else if (type == "whirlpool") {
         return whirlpool_type(size);
-    }
-    else
-    {
+    } else {
         std::cerr << "Error: unknown type" << std::endl;
         exit(1);
     }
