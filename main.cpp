2cef33ca1f8c153466fc88445b3134fe0931485f
ChrisKim <1515748548@qq.com>
2024-04-29 13:12:36 +0800

    [feat]: MNIST主函数

--- main.cpp
+++ main.cpp
@@ -4,30 +4,27 @@
 #include "layer/ActivationLayer.h"
 #include "activation/tanh.h"
 #include "loss/MeanSquaredError.h"
-#include "test/LoadPlane.h"
-#include "test/SavePlaneMesh.h"
+#include "test/TestCaseFile.h"
 
 int main() {
+    std::ios::sync_with_stdio(false);
     Tanh tanh;
 
     MeanSquaredError mse;
     Network network({
-        new FullyConnectedLayer(2, 20),
+        new FullyConnectedLayer(28 * 28, 100),
         new ActivationLayer(&tanh),
-        new FullyConnectedLayer(20, 20),
+        new FullyConnectedLayer(100, 50),
         new ActivationLayer(&tanh),
-        new FullyConnectedLayer(20, 1),
+        new FullyConnectedLayer(50, 10),
         new ActivationLayer(&tanh)
     });
 
-    auto [train_input, train_output] = load_plane(1000, "circle");
-    auto [test_input, test_output] = load_plane(500, "circle");
+    auto [train_input, train_output] = load_testcase("../mnist_train.txt");
+    auto [test_input, test_output] = load_testcase("../mnist_test.txt");
 
-    network.train(train_input, train_output, &mse, 25, 0.001);
-
-    network.evaluate(test_input, test_output, &mse);
-
-    save_plane_mesh_with_data("../test.txt", "../mesh.txt", test_input, test_output, network);
+    network.train(train_input, train_output, &mse, 60, 0.1);
+    network.evaluate(test_input, test_output, &mse, true);
 
     return 0;
 }
