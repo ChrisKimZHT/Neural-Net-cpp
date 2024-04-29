afa79a29c3ff984d93e5125d975cf4b2bd0eae72
ChrisKim <1515748548@qq.com>
2024-04-29 10:17:07 +0800

    [feat]: 主函数

--- main.cpp
+++ main.cpp
@@ -1,54 +1,33 @@
-#include <iostream>
 #include <vector>
-#include <utility>
-#include "activation/Sigmoid.h"
-#include "matrix/Matrix.h"
-#include "test/LoadPlane.h"
-#include "test/TestCaseFile.h"
-#include "test/SavePlaneMesh.h"
 #include "network/Network.h"
-#include "loss/MeanSquaredError.h"
 #include "layer/FullyConnectedLayer.h"
 #include "layer/ActivationLayer.h"
+#include "activation/tanh.h"
+#include "loss/MeanSquaredError.h"
+#include "test/LoadPlane.h"
+#include "test/SavePlaneMesh.h"
 
 int main() {
-    Sigmoid sigmoid;
+    Tanh tanh;
+
     MeanSquaredError mse;
-    Network network(&mse);
-    FullyConnectedLayer layer1(2, 3);
-    network.add_layer(&layer1);
-    ActivationLayer activation1(3, &sigmoid);
-    network.add_layer(&activation1);
-    FullyConnectedLayer layer2(3, 1);
-    network.add_layer(&layer2);
-    ActivationLayer activation2(1, &sigmoid);
-    network.add_layer(&activation2);
+    Network network({
+        new FullyConnectedLayer(2, 20),
+        new ActivationLayer(&tanh),
+        new FullyConnectedLayer(20, 20),
+        new ActivationLayer(&tanh),
+        new FullyConnectedLayer(20, 1),
+        new ActivationLayer(&tanh)
+    });
 
-    std::vector<Matrix> train_input;
-    std::vector<Matrix> train_output;
-    Matrix a(2, 1), b(2, 1), c(2, 1), d(2, 1);
-    a[0][0] = 0;
-    a[1][0] = 0;
-    b[0][0] = 0;
-    b[1][0] = 1;
-    c[0][0] = 1;
-    c[1][0] = 0;
-    d[0][0] = 1;
-    d[1][0] = 1;
-    train_input.push_back(a);
-    train_input.push_back(b);
-    train_input.push_back(c);
-    train_input.push_back(d);
-    Matrix a_out(1, 1), b_out(1, 1), c_out(1, 1), d_out(1, 1);
-    a_out[0][0] = 0;
-    b_out[0][0] = 1;
-    c_out[0][0] = 1;
-    d_out[0][0] = 0;
-    train_output.push_back(a_out);
-    train_output.push_back(b_out);
-    train_output.push_back(c_out);
-    train_output.push_back(d_out);
+    auto [train_input, train_output] = load_plane(1000, "circle");
+    auto [test_input, test_output] = load_plane(500, "circle");
 
-    network.train(train_input, train_output, 10000, 0.1);
+    network.train(train_input, train_output, &mse, 25, 0.001);
+
+    network.evaluate(test_input, test_output, &mse);
+
+    save_plane_mesh_with_data("../test.txt", "../mesh.txt", test_input, test_output, network);
+
     return 0;
 }
