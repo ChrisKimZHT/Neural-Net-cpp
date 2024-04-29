b93fb7fdbb1f95727f5054aafe34aeaf00b5cddb
ChrisKim <1515748548@qq.com>
2024-04-29 20:17:12 +0800

    [feat]: 优化模型结构

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 #include "network/Network.h"
 #include "layer/FullyConnectedLayer.h"
 #include "layer/ActivationLayer.h"
-#include "activation/sigmoid.h"
+#include "activation/Sigmoid.h"
 #include "loss/MeanSquaredError.h"
 #include "test/TestCaseFile.h"
 #include "layer/ConvolutionalLayer.h"
@@ -14,8 +14,10 @@ int main() {
     Network network({
         new ConvolutionalLayer({28, 28}, {3, 3}),
         new ActivationLayer(new Sigmoid),
+        new ConvolutionalLayer({26, 26}, {3, 3}),
+        new ActivationLayer(new Sigmoid),
         new FlattenLayer,
-        new FullyConnectedLayer(26 * 26, 100),
+        new FullyConnectedLayer(24 * 24, 100),
         new ActivationLayer(new Sigmoid),
         new FullyConnectedLayer(100, 10),
         new ActivationLayer(new Sigmoid)
@@ -24,7 +26,7 @@ int main() {
     auto [train_input, train_output] = load_testcase("../mnist_train.txt");
     auto [test_input, test_output] = load_testcase("../mnist_test.txt");
 
-    network.train(train_input, train_output, new MeanSquaredError, 60, 0.1);
+    network.train(train_input, train_output, new MeanSquaredError, 80, 0.1);
     network.evaluate(test_input, test_output, new MeanSquaredError, true);
 
     return 0;
