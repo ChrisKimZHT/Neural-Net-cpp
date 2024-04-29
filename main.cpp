b40fb648fec66b1b8bc629eafb92f8da9d0e9ec7
ChrisKim <1515748548@qq.com>
2024-04-29 16:44:54 +0800

    [feat]: 简化写法

--- main.cpp
+++ main.cpp
@@ -2,29 +2,27 @@
 #include "network/Network.h"
 #include "layer/FullyConnectedLayer.h"
 #include "layer/ActivationLayer.h"
-#include "activation/tanh.h"
+#include "activation/sigmoid.h"
 #include "loss/MeanSquaredError.h"
 #include "test/TestCaseFile.h"
 
 int main() {
     std::ios::sync_with_stdio(false);
-    Tanh tanh;
 
-    MeanSquaredError mse;
     Network network({
         new FullyConnectedLayer(28 * 28, 100),
-        new ActivationLayer(&tanh),
+        new ActivationLayer(new Sigmoid),
         new FullyConnectedLayer(100, 50),
-        new ActivationLayer(&tanh),
+        new ActivationLayer(new Sigmoid),
         new FullyConnectedLayer(50, 10),
-        new ActivationLayer(&tanh)
+        new ActivationLayer(new Sigmoid)
     });
 
     auto [train_input, train_output] = load_testcase("../mnist_train.txt");
     auto [test_input, test_output] = load_testcase("../mnist_test.txt");
 
-    network.train(train_input, train_output, &mse, 60, 0.1);
-    network.evaluate(test_input, test_output, &mse, true);
+    network.train(train_input, train_output, new MeanSquaredError, 60, 0.1);
+    network.evaluate(test_input, test_output, new MeanSquaredError, true);
 
     return 0;
 }
